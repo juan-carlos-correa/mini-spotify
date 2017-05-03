7097806cbbc2c8b00fcb5e955fae7290f9422b7f
Juan Carlos Correa <juan.correa.herrera@gmail.com>
2017-05-02 22:01:54 -0500

    Se inicializa server con express

--- index.js
+++ index.js
@@ -2,8 +2,13 @@
 
 const mongoose = require('mongoose')
 const config = require('./config')
+const app = require('./app')
 
 mongoose.connect(`mongodb://${config.db_host}:${config.db_port}/${config.db_name}`, (err, res) => {
   if (err) throw err
   console.log(`DB en puerto ${config.db_port}`)
+  app.listen(config.server_port, (err) => {
+    if (err) throw err
+    console.log(`server en puerto ${config.server_port}`)
+  })
 })
