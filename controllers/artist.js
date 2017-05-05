13f0305571aa920d080be7d7e586fc9877800498
Juan Carlos Correa <juan.correa.herrera@gmail.com>
2017-05-04 21:45:12 -0500

    Funcion saveArtist

--- controllers/artist.js
+++ controllers/artist.js
@@ -11,6 +11,22 @@ function getArtist (req, res) {
   res.status(200).send({ message: 'Método getArtist' })
 }
 
+function saveArtist (req, res) {
+  const artist = new Artist()
+
+  const params = req.body
+  artist.name = params.name
+  artist.description = params.description
+  artist.image = null
+
+  artist.save((err, artistStored) => {
+    if (err) return res.status(500).send({ message: `Error al guardar el artista: ${err}` })
+    if (!artistStored) return res.status(400).send({ message: 'No se guardó el artista' })
+    res.status(200).send({ message: 'Artista guardado', artistStored })
+  })
+}
+
 module.exports = {
-  getArtist
+  getArtist,
+  saveArtist
 }
--- routes/artist.js
+++ routes/artist.js
@@ -6,5 +6,6 @@ const api = express.Router()
 const mdAuth = require('../middlewares/authenticated')
 
 api.get('/artist', mdAuth.ensureAuth, ArtistController.getArtist)
+api.post('/artist', mdAuth.ensureAuth, ArtistController.saveArtist)
 
 module.exports = api
