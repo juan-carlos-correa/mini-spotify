917893d49921407b7e2d19396cceb27063e61ab0
Juan Carlos Correa <juan.correa.herrera@gmail.com>
2017-05-06 11:26:12 -0500

    Funcion saveSong

--- controllers/song.js
+++ controllers/song.js
@@ -6,6 +6,23 @@ function getSong (req, res) {
   res.status(200).send({ message: 'Ok' })
 }
 
+function saveSong (req, res) {
+  let song = new Song()
+
+  const params = req.body
+  song.name = params.name
+  song.duration = params.duration
+  song.album = params.album
+  song.number = 1
+
+  song.save((err, songStored) => {
+    if (err) return res.status(500).send({ message: `Error al guardar la canción: ${err}` })
+    if (!songStored) return res.status(400).send({ message: 'Error al guardar la canción' })
+    res.status(200).send({ message: 'Canción guardada', songStored })
+  })
+}
+
 module.exports = {
-  getSong
+  getSong,
+  saveSong
 }
--- routes/song.js
+++ routes/song.js
@@ -6,5 +6,6 @@ const api = express.Router()
 const mdAuth = require('../middlewares/authenticated')
 
 api.get('/song', mdAuth.ensureAuth, SongController.getSong)
+api.post('/song', mdAuth.ensureAuth, SongController.saveSong)
 
 module.exports = api
