4c289394de6aa789132900bdadb58fa3fb0fca59
Juan Carlos Correa <juan.correa.herrera@gmail.com>
2017-05-07 16:29:23 -0500

    Funciones uploadSong y getSongFile

--- controllers/song.js
+++ controllers/song.js
@@ -1,6 +1,8 @@
 'use strcict'
 
 const Song = require('../models/song')
+const path = require('path')
+const fs = require('fs')
 
 function getSong (req, res) {
   const songId = req.params.id
@@ -67,10 +69,36 @@ function removeSong (req, res) {
   })
 }
 
+function uploadSong (req, res) {
+  const songId = req.params.id
+  if (!songId) return res.status(500).send({ message: 'No se recibió el id' })
+  if (!req.files) return res.status(300).send({ message: 'No se seleccionó ningún archivo' })
+  const filePath = req.files.file.path.split('\\')
+  const fileName = filePath[2]
+  const extension = fileName.split('.')
+
+  if (extension[1] !== 'mp3' && extension[1] !== 'ogg') return res.status(400).send({ message: 'El archivo debe ser mp3 u ogg' })
+  Song.findByIdAndUpdate(songId, { file: fileName }, (err, songUpdated) => {
+    if (err) return res.status(500).send({ message: `Error: ${err}` })
+    res.status(200).send({ message: 'Canción actualizada', songUpdated })
+  })
+}
+
+function getSongFile (req, res) {
+  const songFile = req.params.songFile
+  const pathFile = `./uploads/songs/${songFile}`
+  fs.exists(pathFile, (exists) => {
+    if (!exists) return res.status(404).send({ message: 'No se encontró la canción' })
+    res.sendFile(path.resolve(pathFile))
+  })
+}
+
 module.exports = {
   getSong,
   saveSong,
   getSongs,
   updateSong,
-  removeSong
+  removeSong,
+  uploadSong,
+  getSongFile
 }
--- routes/song.js
+++ routes/song.js
@@ -4,11 +4,15 @@ const express = require('express')
 const SongController = require('../controllers/song')
 const api = express.Router()
 const mdAuth = require('../middlewares/authenticated')
+const multipart = require('connect-multiparty')
+const mdUpload = multipart({ uploadDir: './uploads/songs' })
 
 api.get('/song/:id', mdAuth.ensureAuth, SongController.getSong)
 api.get('/songs/:id?', mdAuth.ensureAuth, SongController.getSongs)
 api.post('/song', mdAuth.ensureAuth, SongController.saveSong)
 api.put('/song/:id', mdAuth.ensureAuth, SongController.updateSong)
 api.delete('/song/:id', mdAuth.ensureAuth, SongController.removeSong)
+api.post('/upload-song/:id', [mdAuth.ensureAuth, mdUpload], SongController.uploadSong)
+api.get('/get-song/:songFile', SongController.getSongFile)
 
 module.exports = api
