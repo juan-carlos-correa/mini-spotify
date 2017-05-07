d7583d1b8ebcea1ba3500a125b12dabee9fd197e
Juan Carlos Correa <juan.correa.herrera@gmail.com>
2017-05-07 16:36:08 -0500

    Configuracion de headers para la api

--- app.js
+++ app.js
@@ -12,6 +12,14 @@ const app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-type, Accept, Access-Control-Allow-Request-Method')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+  res.header('Allow', 'GET, POST, PUT, DELETE, OPTIONS')
+  next()
+})
+
 app.use('/api', userRoutes)
 app.use('/api', artistRoutes)
 app.use('/api', albumRoutes)
